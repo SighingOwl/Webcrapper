e9fde9b257b6758a528da9809a465ef76ae2a0e1
SigningOwl <whddms1208@gmail.com>
2023-03-18 16:45:43 +0900

    Make URLFomatting.py check the webstie's response

--- URLFormatting.py
+++ URLFormatting.py
@@ -1,3 +1,5 @@
+from requests import get
+
 websites = (
     'google.com',
     'airbnb.com',
@@ -6,7 +8,15 @@ websites = (
     'https://tiktok.com'
 )
 
+results = {}
+
 for website in websites:
-    if not website.startswith('https://'):
+    if not website.startswith('https://'):  #webstie가 'https://'로 시작하면(True) 조건에 충족하지 않고 그렇지 않으면(False)이면 조건에 충족한다.
         website = f'https://{website}'
-    print(website)
+    response = get(website) #  'get' returns response
+    if response.status_code == 200: # http 응답 코드 확인
+        results[website] = "OK"
+    else:
+        results[website] = "FAILED"
+
+print(results)
