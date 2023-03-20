bac32b7df477ea6f05b53da09b6c4d955b9f4c4b
SigningOwl <whddms1208@gmail.com>
2023-03-20 15:08:23 +0900

    Make main.py get job posts from weworkromotely

--- main.py
+++ main.py
@@ -9,4 +9,10 @@ if response.status_code != 200:
     print("Can't request website")
 else:
     soup = BeautifulSoup(response.text, 'html.parser')
-    print(soup.find_all('section', class_="jobs"))
+    jobs = soup.find_all('section', class_="jobs")  #   'class' 대신 'class_'를 사용하는 이유는 'class'는 이미 python 예약어이기 때문
+    for job_section in jobs:
+        job_posts = job_section.find_all('li')
+        job_posts.pop()
+        for post in job_posts:
+            print(post)
+            print('////////////')
