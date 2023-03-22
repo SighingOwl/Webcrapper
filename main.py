2a5d7e9d2cb1b17174ee5e1bf6707398949b509b
SigningOwl <whddms1208@gmail.com>
2023-03-22 19:38:43 +0900

    Make main.py get job from indeed page sources

--- main.py
+++ main.py
@@ -2,12 +2,20 @@ from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from bs4 import BeautifulSoup
 from extractors.wwr import extract_wwr_jobs
+
 '''
 options = Options()
 options.add_argument("--no-sandbox")    # replit에서 selenium을 동작시키기 위한 코드 
 options.add_argument("--disable-dev-shm-usage")    # replit에서 selenium을 동작시키기 위한 코드 
 '''
+
 browser = webdriver.Chrome() # request를 쓰지 않는 이유는 indeed 페이지가 봇 검사를 하므로 request를 사용한 접속을 막고 있다.
 browser.get("https://kr.indeed.com/jobs?q=python&l=&vjk=958ade57ee98b45a")
 
-print(browser.page_source)
+soup = BeautifulSoup(browser.page_source, "html.parser")
+job_list = soup.find('ul', class_='jobsearch-ResultsList')
+jobs = job_list.find_all('li', recursive=False)
+for job in jobs:
+    zone = job.find('div', class_='mosaic-zone')
+    if zone == None:
+        print('job li')
