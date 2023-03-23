ab165d0c3814ca44d17d86924b018e80fc7fc8b6
SigningOwl <whddms1208@gmail.com>
2023-03-23 18:32:38 +0900

    Add templates 'home.html', 'search.html' & main.py extracts jobs and send to search.html

--- README.md
+++ README.md
@@ -5,3 +5,5 @@ webscrapper project developed by python
 > Get user request and get results from indeed & wwr using extratros
 ### extractors
 > There is 2 extractors which are scraping indeed, wwr
+### templates
+> Flask can only find folder named 'templates' and templates should be in same diretory as main.py
--- main.py
+++ main.py
@@ -1,9 +1,19 @@
-from flask import Flask
+from flask import Flask, render_template, request
+from extractors.indeed import extract_indeed_jobs
+from extractors.wwr import extract_wwr_jobs
 
 app = Flask('JobScrapper')
 
 @app.route('/') # '@':decorator, synractic sugar : 문법적 기능은 유지하되 코드 작성 및 읽는 것에서 편의성을 높인 프로그래밍 문법
 def home():
-    return 'hey there!'
+    return render_template('home.html', name='A')   # flask는 render_template을 사용해서 주어진 변수를 template에 적용할 수 있다.
+
+@app.route('/search')
+def search():
+    keyword = request.args.get('keyword')
+    indeed = extract_indeed_jobs(keyword)
+    wwr = extract_wwr_jobs(keyword)
+    jobs = indeed + wwr
+    return render_template('search.html', keyword=keyword, jobs=jobs)
 
 app.run('127.0.0.1')
