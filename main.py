de4d0e70518e0234d09d9b341af1c146e15496e1
SigningOwl <whddms1208@gmail.com>
2023-03-23 20:58:44 +0900

    Complete to develope webscrapper

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, send_file
 from extractors.indeed import extract_indeed_jobs
 from extractors.wwr import extract_wwr_jobs
+from file import save_to_file
 
 app = Flask('JobScrapper')
 
@@ -13,6 +14,8 @@ def home():
 @app.route('/search')
 def search():
     keyword = request.args.get('keyword')
+    if keyword == None or keyword == '':
+        return redirect('/')
     if keyword in db:
         jobs = db[keyword]
     else:
@@ -21,5 +24,16 @@ def search():
         jobs = indeed + wwr
         db[keyword] = jobs
     return render_template('search.html', keyword=keyword, jobs=jobs)
+
+@app.route('/export')
+def export():
+    keyword = request.args.get('keyword')
+    if keyword == None or keyword == '':
+        return redirect('/')
+    if keyword not in db:
+        return redirect(f'/search?keyword={keyword}')
+    save_to_file(keyword, db[keyword])
+    return send_file(f'{keyword}.csv', as_attachment=True)  # 결과를 사용자가 다운로드할 수 있도록 하는 방법
+
 
 app.run('127.0.0.1', port=5001)
